d04ce558cc5c25c0fe5f8c9f0380326f3ae89603
d41dev <d41dev@proton.me>
2026-07-09 15:20:59 +0100

    update: add concept card

---
+++ app/src/main/java/com/fpf/smartscan/ui/components/concepts/ConceptCard.kt
@@ -0,0 +1,104 @@
+package com.fpf.smartscan.ui.components.concepts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.OpenInFull
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.fpf.smartscan.concepts.Concept
+import com.fpf.smartscan.ui.components.common.CircularCheckbox
+
+
+@Composable
+fun ConceptCard(
+    item: Concept,
+    isSelecting: Boolean,
+    isChecked: () -> Boolean,
+    onToggleSelected: (Concept) -> Unit,
+    onItemClick: (Concept) -> Unit,
+    modifier: Modifier = Modifier,
+    onToggleSelectionMode: () -> Unit,
+){
+    val shape = RoundedCornerShape(12.dp)
+
+    Box(
+        modifier = modifier
+            .aspectRatio(0.8f)
+            .padding(4.dp)
+            .clip(shape)
+            .border(1.dp, MaterialTheme.colorScheme.outline.copy(alpha = 0.2f), shape)
+            .combinedClickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = {
+                    if (isSelecting) onToggleSelected(item) else onItemClick(item)
+                },
+                onLongClick = {
+                    if (!isSelecting) {
+                        onToggleSelectionMode()
+                        onToggleSelected(item)
+                    }
+                }
+            )
+    ) {
+        Box(modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+            .padding(16.dp),
+//            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = item.description,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+        if(isSelecting) {
+            CircularCheckbox(
+                checked = isChecked() ,
+                onCheckedChange = { onToggleSelected(item) },
+                modifier = Modifier
+                    .offset(x = 8.dp, y = 8.dp)
+                    .align(Alignment.TopStart),
+            )
+            Box(
+                modifier = Modifier
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) {
+                        onItemClick(item)
+                    }
+                    .offset((-8).dp, (-8).dp)
+                    .align(Alignment.BottomEnd)
+            ) {
+                Icon(Icons.Filled.OpenInFull, contentDescription = "Expand item", modifier = Modifier
+                    .size(20.dp)
+                    .padding(2.dp)
+                    .background(
+                        MaterialTheme.colorScheme.surfaceDim.copy(alpha = 0.5f),
+                        RoundedCornerShape(2.dp)
+                    )
+                )
+            }
+        }
+    }
+}
