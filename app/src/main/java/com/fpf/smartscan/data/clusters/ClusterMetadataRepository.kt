5b73b98d71a5e55f7ecfc86a0ca0b348be52a353
d41dev <d41dev@proton.me>
2026-07-10 11:46:05 +0100

    update: use domain models in repository with internal mapping with entities

--- app/src/main/java/com/fpf/smartscan/data/clusters/ClusterMetadataRepository.kt
+++ app/src/main/java/com/fpf/smartscan/data/clusters/ClusterMetadataRepository.kt
@@ -1,24 +1,28 @@
 package com.fpf.smartscan.data.clusters
 
+import com.fpf.smartscan.data.mappers.toDomain
+import com.fpf.smartscan.data.mappers.toEntity
+import com.fpf.smartscan.media.MediaCollection
 import com.fpf.smartscansdk.core.cluster.ClusterMetadata
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlin.collections.associate
 
 class ClusterMetadataRepository(private val dao: ClusterMetadataDao) {
     suspend fun getAllMetadataAsMap(): Map<Long, ClusterMetadata> = dao.get().associate {
-            it.clusterId to it.toMetadata()
+            it.clusterId to it.toDomain().second
         }
 
-    fun getCollections(): Flow<List<AutoCollectionData>> = dao.getCollections()
-    suspend fun getMetadata(ids: List<Long>): List<ClusterMetadataEntity> = dao.get(ids)
-    suspend fun getMetadata(id: Long): ClusterMetadataEntity? = dao.get(listOf(id)).firstOrNull()
+    fun getCollections(): Flow<List<MediaCollection>> = dao.getCollections().map{collections -> collections.map{it.toDomain()}}
+    suspend fun getMetadata(ids: List<Long>): List<Pair<Long, ClusterMetadata>> = dao.get(ids).map{it.toDomain()}
+    suspend fun getMetadata(id: Long): Pair<Long, ClusterMetadata>? = dao.get(listOf(id)).firstOrNull()?.toDomain()
 
     suspend fun count(minSize: Int = 1): Int = dao.count(minSize)
-    suspend fun insertMetadata(metadataBatch: List<ClusterMetadataEntity>) = dao.insert(metadataBatch)
-    suspend fun insertMetadata(metadata: ClusterMetadataEntity) = dao.insert(listOf(metadata))
+    suspend fun insertMetadata(metadataBatch: List<Pair<Long, ClusterMetadata>>) = dao.insert(metadataBatch.map{it.toEntity()})
+    suspend fun insertMetadata(metadata: Pair<Long, ClusterMetadata>) = dao.insert(listOf(metadata.toEntity()))
 
-    suspend fun updateMetadata(metadataBatch: List<ClusterMetadataEntity>) = dao.update(metadataBatch)
-    suspend fun updateMetadata(metadata: ClusterMetadataEntity) = dao.update(listOf(metadata))
+    suspend fun updateMetadata(metadataBatch: List<Pair<Long, ClusterMetadata>>) = dao.update(metadataBatch.map{it.toEntity()})
+    suspend fun updateMetadata(metadata: Pair<Long, ClusterMetadata>) = dao.update(listOf(metadata.toEntity()))
 
     suspend fun deleteMetadata(ids: List<Long>) = dao.delete(ids)
     suspend fun deleteMetadata(id: Long) = dao.delete(listOf(id))
--- app/src/main/java/com/fpf/smartscan/data/tags/TagRepository.kt
+++ app/src/main/java/com/fpf/smartscan/data/tags/TagRepository.kt
@@ -1,23 +1,28 @@
 package com.fpf.smartscan.data.tags
 
+import com.fpf.smartscan.data.mappers.toDomain
+import com.fpf.smartscan.data.mappers.toEntity
+import com.fpf.smartscan.media.MediaCollection
+import com.fpf.smartscan.tag.Tag
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class TagRepository(private val dao: TagDao) {
-     val allTags: Flow<List<TagEntity>> = dao.getAllFlow()
+     val allTags: Flow<List<Tag>> = dao.getAllFlow().map{tags -> tags.map{it.toDomain()}}
 
-     fun getCollections(): Flow<List<TagCollectionData>> = dao.getCollections()
+     fun getCollections(): Flow<List<MediaCollection>> = dao.getCollections().map{ collections -> collections.map{it.toDomain()}}
 
-     suspend fun getAllTags(): List<TagEntity> = dao.getAll()
+     suspend fun getAllTags(): List<Tag> = dao.getAll().map{it.toDomain()}
 
-     suspend fun getTagsByName(names: List<String>): List<TagEntity> = dao.getByNames(names)
+     suspend fun getTagsByName(names: List<String>): List<Tag> = dao.getByNames(names).map{it.toDomain()}
 
-     suspend fun getTagsById(ids: List<Long>): List<TagEntity> = dao.getByIds(ids)
+     suspend fun getTagsById(ids: List<Long>): List<Tag> = dao.getByIds(ids).map{it.toDomain()}
 
-     suspend fun insertTags(mediaTags: List<TagEntity>): List<Long> = dao.insert(mediaTags)
+     suspend fun insertTags(mediaTags: List<Tag>): List<Long> = dao.insert(mediaTags.map{it.toEntity()})
 
-     suspend fun updateTags(mediaTags: List<TagEntity>) = dao.update(mediaTags)
+     suspend fun updateTags(mediaTags: List<Tag>) = dao.update(mediaTags.map{it.toEntity()})
 
-     suspend fun deleteTags(mediaTags: List<TagEntity>) = dao.delete(mediaTags)
+     suspend fun deleteTags(mediaTags: List<Tag>) = dao.delete(mediaTags.map{it.toEntity()})
 
      suspend fun deleteTagsByName(names: List<String>) = dao.deleteByNames(names)
 
