55c4d66de77892d593a8a07daa5e098fe9c0ffb4
d41dev <d41dev@proton.me>
2026-07-10 19:30:11 +0100

    update: add queries to get media which havent been scanned for concepts using media with description as proxy

--- app/src/main/java/com/fpf/smartscan/data/metadata/MediaMetadataRepository.kt
+++ app/src/main/java/com/fpf/smartscan/data/metadata/MediaMetadataRepository.kt
@@ -10,12 +10,9 @@ class MediaMetadataRepository(
 ) {
     suspend fun insert(items: List<MediaMetadata>) = dao.insert(items.map{it.toEntity()})
 
-    suspend fun insert(item: MediaMetadata) = dao.insert(item.toEntity())
-
     suspend fun update(items: List<MediaMetadata>) = dao.update(items.map{it.toEntity()})
-
-    suspend fun update(item: MediaMetadata) = dao.update(item.toEntity())
     suspend fun getUnclusteredItemIds(): Map<Long, MediaType> = dao.getUnclusteredItemIds().associate { it.id to it.type }
+
     suspend fun getByIds(mediaIds: List<Long>, type: MediaType): List<MediaMetadata> = dao.getByIds(mediaIds, type).map{it.toDomain()}
     suspend fun getByType(type: MediaType): List<MediaMetadata> = dao.getByType(type).map{it.toDomain()}
     suspend fun getIdsByType(type: MediaType): List<Long> = dao.getIdsByType(type)
@@ -24,9 +21,12 @@ class MediaMetadataRepository(
     suspend fun getByTag(tagId: Long, type: MediaType, limit: Int, offset: Int): List<MediaMetadata> = dao.getByTag(tagId, type, limit, offset).map{it.toDomain()}
     suspend fun getByTag(tagId: Long, type: MediaType): List<MediaMetadata> = dao.getByTag(tagId, type).map{it.toDomain()}
     suspend fun getByTag(tagId: Long, type: MediaType, startDate: Long?, endDate: Long?): List<MediaMetadata> = dao.getByTag(tagId, type, startDate, endDate).map{it.toDomain()}
+
+    suspend fun getByTagsWithoutDescription(tagIds: List<Long>, mediaType: MediaType): List<MediaMetadata> = dao.getByTagsWithoutDescription(tagIds, mediaType).map{it.toDomain()}
     suspend fun getByCluster(clusterId: Long, limit: Int, offset: Int): List<MediaMetadata> = dao.getByCluster(clusterId, limit, offset).map{it.toDomain()}
     suspend fun getByCluster(clusterId: Long): List<MediaMetadata> = dao.getByCluster(clusterId).map{it.toDomain()}
     suspend fun getByCluster(clusterId: Long, type: MediaType, limit: Int, offset: Int): List<MediaMetadata> = dao.getByCluster(clusterId, type, limit, offset).map{it.toDomain()}
+    suspend fun getByClustersWithoutDescription(clusterIds: List<Long>, mediaType: MediaType): List<MediaMetadata> = dao.getByClustersWithoutDescription(clusterIds, mediaType).map{it.toDomain()}
 
     suspend fun getByConcept(conceptId: Long, limit: Int, offset: Int): List<MediaMetadata> = dao.getByConcept(conceptId, limit, offset).map{it.toDomain()}
     suspend fun getByConcept(conceptId: Long): List<MediaMetadata> = dao.getByConcept(conceptId).map{it.toDomain()}
