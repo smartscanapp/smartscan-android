6d3e9cb6aac35a2216f5e0d5795e848496ade204
d41dev <d41dev@proton.me>
2026-07-11 21:04:25 +0100

    update: add items size metadata and remove aspect ratio

--- app/src/main/java/com/fpf/smartscan/ui/components/concepts/ConceptCard.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/concepts/ConceptCard.kt
@@ -5,14 +5,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lightbulb
 import androidx.compose.material.icons.filled.OpenInFull
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -37,20 +44,27 @@ fun ConceptCard(
     onItemClick: (Concept) -> Unit,
     modifier: Modifier = Modifier,
     onToggleSelectionMode: () -> Unit,
-){
+) {
     val shape = RoundedCornerShape(12.dp)
 
     Box(
         modifier = modifier
-            .aspectRatio(0.8f)
             .padding(4.dp)
             .clip(shape)
-            .border(1.dp, MaterialTheme.colorScheme.outline.copy(alpha = 0.2f), shape)
+            .border(
+                1.dp,
+                MaterialTheme.colorScheme.outline.copy(alpha = 0.2f),
+                shape
+            )
             .combinedClickable(
                 indication = null,
                 interactionSource = remember { MutableInteractionSource() },
                 onClick = {
-                    if (isSelecting) onToggleSelected(item) else onItemClick(item)
+                    if (isSelecting) {
+                        onToggleSelected(item)
+                    } else {
+                        onItemClick(item)
+                    }
                 },
                 onLongClick = {
                     if (!isSelecting) {
@@ -60,24 +74,36 @@ fun ConceptCard(
                 }
             )
     ) {
-        Box(modifier = modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surfaceContainer)
-            .padding(16.dp),
-//            contentAlignment = Alignment.Center
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surfaceContainer)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = item.description,
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 5,
                 overflow = TextOverflow.Ellipsis
             )
+
+            Spacer(modifier=Modifier.height(16.dp))
+
+            Text(
+                text = "${item.size} items",
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
         }
-        if(isSelecting) {
+
+        if (isSelecting) {
             CircularCheckbox(
-                checked = isChecked() ,
+                checked = isChecked(),
                 onCheckedChange = { onToggleSelected(item) },
                 modifier = Modifier
                     .offset(x = 8.dp, y = 8.dp)
-                    .align(Alignment.TopStart),
+                    .align(Alignment.TopStart)
             )
         }
     }
