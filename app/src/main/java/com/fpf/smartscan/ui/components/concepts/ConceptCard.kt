99c3a1e9a4440301cf95009b683cdcfd9ca3ea49
d41dev <d41dev@proton.me>
2026-07-11 22:15:01 +0100

    update: make first character of description uppercase

--- app/src/main/java/com/fpf/smartscan/ui/components/concepts/ConceptCard.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/concepts/ConceptCard.kt
@@ -2,28 +2,18 @@ package com.fpf.smartscan.ui.components.concepts
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lightbulb
-import androidx.compose.material.icons.filled.OpenInFull
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -86,7 +76,7 @@ fun ConceptCard(
         ) {
             Text(
                 modifier=Modifier.weight(1f, fill = false),
-                text = item.description,
+                text = item.description.replaceFirstChar { it.uppercase() },
                 style = MaterialTheme.typography.bodyLarge,
                 overflow = TextOverflow.Ellipsis
             )
