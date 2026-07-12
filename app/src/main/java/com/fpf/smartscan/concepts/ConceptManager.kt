207aa6cf7dfa8ae8807653aaa1d1ceb9e9acb743
d41dev <d41dev@proton.me>
2026-07-12 08:47:05 +0100

    fix: update concept embedding when editting

--- app/src/main/java/com/fpf/smartscan/concepts/ConceptManager.kt
+++ app/src/main/java/com/fpf/smartscan/concepts/ConceptManager.kt
@@ -9,6 +9,7 @@ import com.fpf.smartscansdk.core.embeddings.TextEmbeddingProvider
 import com.fpf.smartscansdk.core.embeddings.toQInt8Embed
 
 class ConceptManager(
+    private val similarityThreshold: Float,
     private val textEmbedder: TextEmbeddingProvider,
     private val conceptRepository: ConceptRepository,
     private val conceptCrossRefRepository: ConceptCrossRefRepository,
@@ -17,34 +18,38 @@ class ConceptManager(
 ) {
     private var idCount: Long = 0L
 
-    suspend fun createConcept(description: String): Concept{
+    suspend fun createConcept(description: String){
         if(!textEmbedder.isInitialized()) textEmbedder.initialize()
         val rawEmbedding = textEmbedder.embed(description)
         val concept = Concept(id = generateId(), description = description, size = 0)
         conceptRepository.insertConcept(concept)
         conceptEmbedStore.add(listOf(StoredEmbedding(id = concept.id, date = System.currentTimeMillis(), rawEmbedding.toQInt8Embed())))
         ++idCount
-        return concept
     }
 
-    suspend fun editConcept(concept: Concept, newDescription: String): Concept{
+    suspend fun editConcept(concept: Concept, newDescription: String){
+        if(!textEmbedder.isInitialized()) textEmbedder.initialize()
         val updatedConcept = concept.copy(description = newDescription, updatedAt = System.currentTimeMillis())
         conceptRepository.upsertConcept(updatedConcept)
-        return updatedConcept
+
+        val rawEmbedding = textEmbedder.embed(newDescription)
+        val updatedEmbed = StoredEmbedding(id = updatedConcept.id, date = System.currentTimeMillis(), rawEmbedding.toQInt8Embed())
+        conceptEmbedStore.update(listOf(updatedEmbed))
+        findAndUpdateMediaMatchingConcept(updatedConcept)
     }
 
     suspend fun deleteConcepts(concepts: List<Concept>){
         conceptRepository.deleteConcepts(concepts)
     }
 
-    suspend fun findMediaMatchingConcept(concept: Concept, threshold: Float): Map<Long, MediaType>{
+    suspend fun findMediaMatchingConcept(concept: Concept): Map<Long, MediaType>{
         val conceptEmbedding = conceptEmbedStore.get(listOf(concept.id)).firstOrNull()?: return emptyMap()
-        val result = imageConceptEmbedStore.query(conceptEmbedding.embedding, Int.MAX_VALUE, threshold)
+        val result = imageConceptEmbedStore.query(conceptEmbedding.embedding, Int.MAX_VALUE, similarityThreshold)
         return result.ids.associateWith { MediaType.IMAGE } // will add video support later
     }
 
-    suspend fun findAndUpdateMediaMatchingConcept(concept: Concept, threshold: Float){
-        val mediaMatchesMap = findMediaMatchingConcept(concept, threshold)
+    suspend fun findAndUpdateMediaMatchingConcept(concept: Concept){
+        val mediaMatchesMap = findMediaMatchingConcept(concept)
         addMediaToConcept(mediaMatchesMap, concept.id)
     }
 
