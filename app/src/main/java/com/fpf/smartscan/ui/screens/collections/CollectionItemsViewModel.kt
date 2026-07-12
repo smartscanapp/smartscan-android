ca9b7e680cea9fe0e4b017222fabf46016bcf28c
d41dev <d41dev@proton.me>
2026-07-12 07:54:47 +0100

    fix: imports

--- app/src/main/java/com/fpf/smartscan/ui/screens/collections/CollectionItemsViewModel.kt
+++ app/src/main/java/com/fpf/smartscan/ui/screens/collections/CollectionItemsViewModel.kt
@@ -27,7 +27,6 @@ import com.fpf.smartscan.events.CollectionItemEventType
 import com.fpf.smartscan.media.CollectionType
 import com.fpf.smartscan.media.MediaItem
 import com.fpf.smartscan.media.MediaType
-import com.fpf.smartscan.media.mediaIdToUri
 import com.fpf.smartscan.media.openImageInGallery
 import com.fpf.smartscan.media.openVideoInGallery
 import com.fpf.smartscan.media.onMediaLoadingError
