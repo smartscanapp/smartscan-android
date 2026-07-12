d0ec0ac2529abc4f941688b97e727de17993083c
d41dev <d41dev@proton.me>
2026-07-12 20:48:19 +0100

    fix: zIndex

--- app/src/main/java/com/fpf/smartscan/ui/components/common/ActionRowWtihFade.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/common/ActionRowWtihFade.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 
 @Composable
 fun ActionRowWithFade(
@@ -33,6 +34,7 @@ fun ActionRowWithFade(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .zIndex(-1f)
                 .graphicsLayer {
                     this.alpha = alpha
                     this.translationY = translationY
