fdb236dc27a106a29daa212e903d02b2791681c2
d41dev <d41dev@proton.me>
2026-07-13 16:39:30 +0100

    update: support optionally hiding if disabled

--- app/src/main/java/com/fpf/smartscan/ui/action/ActionConfig.kt
+++ app/src/main/java/com/fpf/smartscan/ui/action/ActionConfig.kt
@@ -6,5 +6,6 @@ data class ActionConfig(
     val label: String,
     val onClick: () -> Unit,
     val enabled: Boolean = true,
+    val hideIfDisabled: Boolean = false,
     val icon: ImageVector? = null,
 )
