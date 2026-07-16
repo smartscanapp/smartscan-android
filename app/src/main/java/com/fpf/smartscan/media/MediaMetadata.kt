f7e82cc084d4f7b2b6a249598c7ad26c15dce76f
d41dev <d41dev@proton.me>
2026-07-16 20:54:56 +0100

    update: added MediaItem to MediaMetadata in mappers

--- app/src/main/java/com/fpf/smartscan/media/MediaMetadata.kt
+++ app/src/main/java/com/fpf/smartscan/media/MediaMetadata.kt
@@ -6,10 +6,3 @@ data class MediaMetadata(
     val dateAdded: Long,
     val description: String? = null
 )
-
-fun MediaMetadata.toItem(): MediaItem = MediaItem(
-    id=id,
-    type=type,
-    dateAdded=dateAdded,
-    description=description
-)
