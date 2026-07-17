b93c16a7ed467fe3a0dea96c99303b5b1f1ada1a
d41dev <d41dev@proton.me>
2026-07-17 17:49:29 +0100

    fix: updated clusterCrossRefRepo to correctly cache both images and videos, avoiding clashes

--- app/src/main/java/com/fpf/smartscan/data/clusters/ClusterCrossRefRepository.kt
+++ app/src/main/java/com/fpf/smartscan/data/clusters/ClusterCrossRefRepository.kt
@@ -6,7 +6,7 @@ import com.fpf.smartscan.data.mappers.toEntity
 import com.fpf.smartscan.media.MediaType
 
 class ClusterCrossRefRepository(private val dao: ClusterCrossRefDao) {
-    private var clusterToMediaIdsMap: MutableMap<Long, MutableSet<Long>> = mutableMapOf()
+    private var clusterToMediaIdsMap: MutableMap<Pair<Long, MediaType>, MutableSet<Long>> = mutableMapOf()
     private var refreshCache: Boolean = false
 
     suspend fun getAllCrossRefs(): List<ClusterCrossRef> = dao.getAll().map{it.toDomain()}
@@ -25,10 +25,7 @@ class ClusterCrossRefRepository(private val dao: ClusterCrossRefDao) {
     suspend fun count() = dao.count()
     suspend fun count(clusterId: Long) = dao.countByClusterId(clusterId)
 
-    //NOTE: may require changing foreign key is now (id, type) though this may not be necessary
-    // because its only used in search and search around handles media types separate meaninng any collision between
-    // MediaStre video ids and image ids arent possible.
-    suspend fun getClusterToMediaIdsMap(): Map<Long, MutableSet<Long>> {
+    suspend fun getClusterToMediaIdsMap(): Map<Pair<Long, MediaType>, MutableSet<Long>> {
         if(refreshCache){
             clusterToMediaIdsMap.clear()
             refreshCache = false
@@ -36,7 +33,7 @@ class ClusterCrossRefRepository(private val dao: ClusterCrossRefDao) {
         if (clusterToMediaIdsMap.isNotEmpty()) return clusterToMediaIdsMap
 
         for(ref in getAllCrossRefs()){
-            clusterToMediaIdsMap.computeIfAbsent(ref.clusterId) { HashSet() }.add(ref.mediaId)
+            clusterToMediaIdsMap.computeIfAbsent(Pair(ref.clusterId, ref.mediaType)) { HashSet() }.add(ref.mediaId)
         }
         return clusterToMediaIdsMap
     }
