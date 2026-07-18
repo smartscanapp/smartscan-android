2735a508e9c344cba0e2b671b31cc4660c9f4ec7
d41dev <d41dev@proton.me>
2026-07-18 08:55:13 +0100

    refactor: default to desc

--- app/src/main/java/com/fpf/smartscan/search/SortBy.kt
+++ app/src/main/java/com/fpf/smartscan/search/SortBy.kt
@@ -3,6 +3,6 @@ package com.fpf.smartscan.search
 sealed interface SortBy {
     val ascending: Boolean
 
-    data class Date(override val ascending: Boolean = true): SortBy
-    data class Similarity(override val ascending: Boolean = true): SortBy
+    data class Date(override val ascending: Boolean = false): SortBy
+    data class Similarity(override val ascending: Boolean = false): SortBy
 }
