c343385ddb7f1a6f7f92e06c1d6deb9c0641ade6
d41dev <d41dev@proton.me>
2026-07-19 22:57:31 +0100

    update: extend search filter

--- app/src/main/java/com/fpf/smartscan/search/SearchFilter.kt
+++ app/src/main/java/com/fpf/smartscan/search/SearchFilter.kt
@@ -4,6 +4,9 @@ import com.fpf.smartscan.media.MediaType
 
 data class SearchFilter(
     val similarity: Float? = null,
-    val mediaType: MediaType? = null
+    val mediaType: MediaType? = null,
+    val tag: String? = null,
+    val startDate: Long? = null,
+    val endDate: Long? = null,
 )
 
