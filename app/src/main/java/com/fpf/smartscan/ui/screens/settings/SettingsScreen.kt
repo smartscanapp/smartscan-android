57f3aa3a537ec538e67c85c2b34a0da8876401d1
d41dev <d41dev@proton.me>
2026-08-04 11:06:11 +0100

    refactor: move allowed folders to 'scan' section of settings

--- app/src/main/java/com/fpf/smartscan/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/fpf/smartscan/ui/screens/settings/SettingsScreen.kt
@@ -121,11 +121,6 @@ fun SettingsScreen(
     )
 
     val searchSettingActions: List<SettingActionConfig> = listOf(
-        SettingActionConfig.Button(
-            label = stringResource(id = R.string.setting_allowed_folders),
-            onClick = { onNavigate(Routes.settingsDetail(SettingsRoutes.ALLOWED_FOLDERS)) },
-            description = stringResource(R.string.setting_searchable_folders_description)
-        ),
         SettingActionConfig.Switch(
             label=stringResource(R.string.setting_auto_open_gallery),
             checked = appSettings.enableDirectGalleryOpen,
@@ -201,6 +196,11 @@ fun SettingsScreen(
     )
 
     val scanSettingActions: List<SettingActionConfig> = listOf(
+        SettingActionConfig.Button(
+            label = stringResource(id = R.string.setting_allowed_folders),
+            onClick = { onNavigate(Routes.settingsDetail(SettingsRoutes.ALLOWED_FOLDERS)) },
+            description = stringResource(R.string.setting_searchable_folders_description)
+        ),
         SettingActionConfig.Button(
             label = stringResource(id = R.string.scan_action),
             onClick = { onScanRefresh() },
