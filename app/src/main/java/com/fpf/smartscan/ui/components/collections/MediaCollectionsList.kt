76163fb8ff05edb0817d516984e3efc696c13a54
d41dev <d41dev@proton.me>
2026-08-12 00:34:42 +0100

    update: add collection card

--- app/src/main/java/com/fpf/smartscan/ui/components/collections/MediaCollectionsList.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/collections/MediaCollectionsList.kt
@@ -4,28 +4,19 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.border
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -37,19 +28,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.fpf.smartscan.core.media.MediaCollection
-import com.fpf.smartscan.core.media.MediaType
-import com.fpf.smartscan.ui.components.common.CircularCheckbox
-import com.fpf.smartscan.ui.components.media.ImageDisplay
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -133,58 +117,13 @@ fun MediaCollectionsList(
                 items = items,
                 key = { it.id }
             ) { item ->
-                val shape = RoundedCornerShape(12.dp)
-
-                Column{
-                    Box(
-                        modifier = Modifier
-                            .aspectRatio(1f)
-                            .padding(4.dp)
-                            .clip(shape)
-                            .border(1.dp, Color.Gray.copy(alpha = 0.2f), shape)
-                            .combinedClickable(
-                                indication = null,
-                                interactionSource = remember { MutableInteractionSource() },
-                                onClick = { onItemClick(item) },
-                                onLongClick =if (isSelecting) {
-                                    null
-                                } else {
-                                    { onLongItemClick?.invoke(item) }
-                                }
-                            )
-                    ) {
-                        ImageDisplay(
-                            uri = item.thumbNail,
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop,
-                            mediaType = MediaType.IMAGE
-                        )
-
-                        if (isSelecting) {
-                            CircularCheckbox(
-                                checked = item in selectedItems || (selectAll && item !in excludedItems),
-                                onCheckedChange = { onItemClick(item) },
-                                modifier = Modifier
-                                    .offset(x = 8.dp, y = 8.dp)
-                                    .align(Alignment.TopStart),
-                            )
-                        }
-                    }
-                    Text(
-                        text = item.name.replaceFirstChar { char -> char.uppercase() },
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding( start = 2.dp),
-                        color= MaterialTheme.colorScheme.onSurface,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                    Text(
-                        text = item.size.toString(),
-                        style = MaterialTheme.typography.bodySmall,
-                        modifier = Modifier.padding(bottom = 4.dp, start = 2.dp),
-                        color= MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
-                    )
-                }
+                CollectionCard(
+                    item =item,
+                    isSelecting = isSelecting,
+                    onItemClick=onItemClick,
+                    onLongItemClick =onLongItemClick,
+                    isChecked = {item in selectedItems || (selectAll && item !in excludedItems)}
+                )
             }
         }
 
