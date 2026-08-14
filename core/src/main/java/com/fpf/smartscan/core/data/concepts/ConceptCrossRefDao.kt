0796dd92196f3f3a612e9188f6fd053afd10e477
d41dev <d41dev@proton.me>
2026-08-14 20:29:24 +0100

    update: add query to set cross ref hidden

--- core/src/main/java/com/fpf/smartscan/core/data/concepts/ConceptCrossRefDao.kt
+++ core/src/main/java/com/fpf/smartscan/core/data/concepts/ConceptCrossRefDao.kt
@@ -15,11 +15,23 @@ interface ConceptCrossRefDao {
     @Query("SELECT * FROM concept_crossref")
     suspend fun get(): List<ConceptCrossRefEntity>
 
+    @Query("SELECT * FROM concept_crossref WHERE conceptId = :conceptId AND mediaId =:mediaId AND mediaType = :mediaType")
+    suspend fun get(conceptId: Long, mediaId: Long, mediaType: MediaType): ConceptCrossRefEntity?
+
     @Query("SELECT * FROM concept_crossref WHERE conceptId in (:ids)")
     suspend fun getByConceptIds(ids: List<Long>): List<ConceptCrossRefEntity>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(crossRefs: List<ConceptCrossRefEntity>)
+
+    @Query("""
+        UPDATE concept_crossref
+        SET isHidden = :isHidden
+        WHERE mediaId = :mediaId
+          AND mediaType = :mediaType
+          AND conceptId = :conceptId
+    """)
+    suspend fun setCrossRefHidden(mediaId: Long, mediaType: MediaType, conceptId: Long, isHidden: Boolean)
 
     @Query("DELETE FROM concept_crossref")
     suspend fun clear()
--- core/src/main/java/com/fpf/smartscan/core/data/concepts/ConceptCrossRefRepository.kt
+++ core/src/main/java/com/fpf/smartscan/core/data/concepts/ConceptCrossRefRepository.kt
@@ -8,6 +8,9 @@ import com.fpf.smartscan.core.media.MediaType
 class ConceptCrossRefRepository(private val dao: ConceptCrossRefDao) {
     suspend fun getAllCrossRefs(): List<ConceptCrossRef> = dao.get().map{it.toDomain()}
     suspend fun getByConceptIds(ids: List<Long>):  List<ConceptCrossRef> = dao.getByConceptIds(ids).map{it.toDomain()}
+    suspend fun setCrossRefHidden(mediaId: Long, mediaType: MediaType, conceptId: Long, isHidden: Boolean) = dao.setCrossRefHidden(mediaId, mediaType, conceptId, isHidden)
+    suspend fun getCrossRef(conceptId: Long, mediaId: Long, mediaType: MediaType) = dao.get(conceptId, mediaId, mediaType )
+
     suspend fun insertConceptCrossRefs(crossRefs: List<ConceptCrossRef>) = dao.insert(crossRefs.map { it.toEntity() })
     suspend fun clear() = dao.clear()
     suspend fun count() = dao.count()
